d97b6d663be08af76da16b0e37343578c0734748
Sebroar <96085630+Sebroar@users.noreply.github.com>
2021-12-15 18:02:36 +0100

    Upload local changes
    Added some resource pack files

--- src/main/java/sebroar/discovery/init/MapsInit.java
+++ src/main/java/sebroar/discovery/init/MapsInit.java
@@ -9,13 +9,8 @@ import sebroar.discovery.item.ChiselItem;
 public class MapsInit {
 	public static void setup() {
 		setupPurificationAltar();
-		setupChisel();
 	}
 	private static void setupPurificationAltar() {
 		PurificationAltarBlock.PURIFIABLES.put(DiscoveryItems.ABYSSIUM.get(), new ItemStack(DiscoveryItems.PURIFIED_ABYSSIUM.get()));
-	}
-	private static void setupChisel() {
-		ChiselItem.ABYSSIUM.put(DiscoveryBlocks.GRIMSTONE.get(), DiscoveryItems.ABYSSIUM.get());
-		ChiselItem.CHISELABLES.put(DiscoveryBlocks.ABYSSIUM_ORE.get(), ChiselItem.ABYSSIUM);
 	}
 }
---
+++ src/main/java/sebroar/discovery/item/DiscoveryToolType.java
@@ -0,0 +1,7 @@
+package sebroar.discovery.item;
+
+import net.minecraftforge.common.ToolType;
+
+public class DiscoveryToolType {
+	public static final ToolType CHISEL = ToolType.get("discovery_chisel");
+}
